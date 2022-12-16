26f6b11172ad64d28b1584de1b52fa61fc005212
Matteo Aldeghi <maldeghi@google.com>
2022-12-16 09:36:54 -0800

    add license header (with internal files removed)

--- xcloud/abfe_base_protocol/main.py
+++ xcloud/abfe_base_protocol/main.py
@@ -1,3 +1,17 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 r"""The main program for running gromacs ABFE calculations with
 a non-equilibrium protocol and Boresch restraints.
 """
@@ -306,7 +320,7 @@ def run_all_transition_tprs_with_early_stopping(tpr_files: List, pmegpu: bool = 
       # Run analysis and get precision.
       _patience = _evaluate_precision_and_update_patience(target_precision, _patience)
       
-    logging.info(f"  Terminating mdrun execution for {env} non-equil simulations")
+    logging.info(f"  Terminating execution of non-equilibrium transitions in {env} environment")
 
   # Run both water and protein sims until precision reached or run out of transitions.
   _execute_sims_with_early_stopping_loop(env='water', 
